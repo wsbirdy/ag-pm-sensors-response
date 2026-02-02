59069420fa8b9f9d414397f252bd40129c0eccff
Sirawat Wongmonta <birdy@airgradient.com>
2026-02-02 15:33:39 +0700

    feat: auto port detection for esp32 memory extration.

--- python_script/extract_memory.py
+++ python_script/extract_memory.py
@@ -1,21 +1,46 @@
 import esptool
+import sys
+import serial.tools.list_ports
+
+def get_esp_port():
+    # Find all potential USB serial ports
+    ports = [p.device for p in serial.tools.list_ports.comports() if "USB" in p.description or "UART" in p.description]
+    
+    if not ports:
+        print("❌ No ESP32 devices found. Please check your connection.")
+        sys.exit(1)
+    
+    if len(ports) == 1:
+        print(f"✅ Found device on: {ports[0]}")
+        return ports[0]
+    
+    # If multiple are found, let the user choose
+    print("\nMultiple devices detected:")
+    for i, port in enumerate(ports):
+        print(f"[{i}] {port}")
+    
+    choice = input(f"Select port (0-{len(ports)-1}): ")
+    try:
+        return ports[int(choice)]
+    except (ValueError, IndexError):
+        print("Invalid selection.")
+        sys.exit(1)
 
 # Configuration from your partition table
-PORT = '/dev/cu.usbmodem21201'
+PORT = get_esp_port() #'/dev/cu.usbmodem21201'
 BAUD = 460800   #115200, 460800
 START_OFFSET = '0x270000'   # Start of LittleFS
 SIZE_HEX = '0x180000'       # Size of LittleFS (1.5MB)
 OUTPUT_FILE = './output_folder/littlefs_raw.bin'
 
-# Updated command for esptool v5.1.0
+# Updated command for esptool
 cmd_args = [
     '--port', PORT,
-    '--baud', str(BAUD),
+    '--baud', str(BAUD), 
     'read-flash', 
     START_OFFSET, 
     SIZE_HEX, 
-    OUTPUT_FILE
-]
+    OUTPUT_FILE]
 
 print(f"Attempting to extract LittleFS from {START_OFFSET}...")
 
